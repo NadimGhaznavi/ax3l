0163fd2b55dc9ac41f08b97a353d2ba972be70b2
Nadim Ghaznavi <nghaznavi@gmail.com>
2026-09-12 03:15:30 -0400

    Feat: Bootstrapping

--- ax3l/constants/DLlama.py
+++ ax3l/constants/DLlama.py
@@ -4,3 +4,7 @@ class DLlama:
     BASE_DIR: Final[str] = "/opt/prod/llama.cpp"
     BIN_DIR: Final[str] = "bin"
     SERVER: Final[str] = "llama-server"
+
+    PORT: Final[int] = 27770
+    PORT_DEV: Final[int] = 27768
+    PORT_QA: Final[int] = 27769

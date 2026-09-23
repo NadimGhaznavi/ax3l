44292c2ff8fb1f206ac84860631867e45e9378c3
Nadim Ghaznavi <nghaznavi@gmail.com>
2026-09-23 17:02:00 -0400

    Release 1.6.8: Human Friendly Status

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.6.8] - 2026-09-23 @ 17:02
+
 ### Changed
 
 - Format Report Server Status scores and counters with comma thousands separators.
--- ax3l/constants/DAx3l.py
+++ ax3l/constants/DAx3l.py
@@ -4,7 +4,7 @@ class DAx3l:
     RAW_LOGS_ENABLED: Final[bool] = False
     HTTP_TIMEOUT_SECONDS: Final[int] = 300
 
-    VERSION: Final[str] = "1.6.7"
+    VERSION: Final[str] = "1.6.8"
 
     BASE_DIR: Final[str] = "/opt/prod/ax3l"
     BASE_DIR_QA: Final[str] = "/opt/qa/ax3l"
